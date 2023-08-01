a7235e3a6f6b6c151bfd9a2b6598a852dd943a7f
MonalisaBasa <monalisa.basa.mb@gmail.com>
2023-08-01 18:47:16 +0530

    Dynamic styling deliverable 1

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,34 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const[isvalid, setisvalid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if(event.target.value.trim().length >0){
+      setisvalid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    // Trim is inbuild method used to remove white space from starting and ending
+    if(enteredValue.trim().length === 0){
+      setisvalid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{color: !isvalid ? 'red' : 'black'}}>Course Goal</label>
+        <input 
+        style={{borderColor: !isvalid ? 'red' : '#ccc',
+        background: !isvalid ? 'Salmon' : 'transparent'
+      }}
+        type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
